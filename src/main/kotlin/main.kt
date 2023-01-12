04c7f2f1d910fc5cf24a9d58b537b303e93bc531
VOSESE <vssbaks@mail.ru>
2023-01-12 11:14:11 +0300

    Editing

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,15 @@
 fun main() {
-    fun comission(typeCard: String = "VK Pay", history: Int = 0, amount: Int): Any {
-        return when (typeCard) {
-            "VK Pay" -> 0
-            "MasterCard" -> if (amount + history < 75_000) 0 else (amount * 0.006 + 20)
-            "Maestro" -> if (amount + history < 75_000) 0 else (amount * 0.006 + 20)
-            "Visa" -> if (amount >= 4666.666666667) (amount * 0.0075).toFloat() else 35
-            "Мир" -> if (amount >= 4666.666666667) (amount * 0.0075).toFloat() else 35
-            else -> "Error"
-        }
-    }
-
-    val result = comission(amount = 355000)
+    val result = comission(amount = 75_000)
     println("Комиссия: $result")
+}
 
+fun comission(typeCard: String = "VK Pay", history: Int = 0, amount: Int): Any {
+    return when (typeCard) {
+        "VK Pay" -> 0
+        "MasterCard" -> if (amount + history < 75_000) 0 else (amount * 0.006 + 20)
+        "Maestro" -> if (amount + history < 75_000) 0 else (amount * 0.006 + 20)
+        "Visa" -> if (amount >= 4666.666666667) (amount * 0.0075).toFloat() else 35
+        "Мир" -> if (amount >= 4666.666666667) (amount * 0.0075).toFloat() else 35
+        else -> "Error"
+    }
 }
